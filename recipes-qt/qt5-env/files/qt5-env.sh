44975863c1bd28f4a11168b5690e9b91b02ffa6b
Scott Ellis <scott@jumpnowtek.com>
2016-11-09 12:53:38 -0500

    qt5-env: Add example for using tslib

--- recipes-qt/qt5-env/files/qt5-env.sh
+++ recipes-qt/qt5-env/files/qt5-env.sh
@@ -3,3 +3,9 @@
 export PATH=${PATH}:/usr/bin/qt5
 
 export QT_QPA_PLATFORM=linuxfb
+
+# to use tslib for input (no multi-touch)
+#export QT_QPA_FB_TSLIB=1
+
+# replace with correct /dev/input/eventX if this is wrong
+#export TSLIB_TSDEVICE=/dev/input/touchscreen0
--- recipes-qt/qt5-env/qt5-env.bb
+++ recipes-qt/qt5-env/qt5-env.bb
@@ -3,7 +3,7 @@ SUMMARY = "Add Qt5 bin dir to PATH"
 LICENSE = "GPL-2.0"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=801f80980d171dd6425610833a22dbe6"
 
-PR = "r1"
+PR = "r2"
 
 SRC_URI = "file://qt5-env.sh"
 
