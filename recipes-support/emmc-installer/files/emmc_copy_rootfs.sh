221c66ef436c00edd2542e031f85e0e33c527030
Scott Ellis <scott@jumpnowtek.com>
2018-10-20 10:15:33 -0400

    emmc-installer: Add EXTRACT_UNSAFE_SYMLINKS to untar command

--- recipes-support/emmc-installer/files/emmc_copy_rootfs.sh
+++ recipes-support/emmc-installer/files/emmc_copy_rootfs.sh
@@ -21,38 +21,38 @@ else
 fi
 
 if [ "x${1}" = "x" ]; then
-	echo -e "\nUsage: ${0} <image-name> [<hostname>]\n"
-	exit 0
+    echo -e "\nUsage: ${0} <image-name> [<hostname>]\n"
+    exit 0
 fi
 
 if [ ! -d /media ]; then
-	echo "Mount point /media does not exist"
-	exit 1
+    echo "Mount point /media does not exist"
+    exit 1
 fi
 
 if [ ! -b $DEV ]; then
-	echo "Block device not found: $DEV"
-	exit 1
+    echo "Block device not found: $DEV"
+    exit 1
 fi
 
 if [ -f "${1}" ]; then
-	FULLPATH="${1}"
+    FULLPATH="${1}"
 elif [ -f "${SRCDIR}/${1}" ]; then
-	FULLPATH="${SRCDIR}/${1}"
+    FULLPATH="${SRCDIR}/${1}"
 elif [ -f "${SRCDIR}/${1}-${MACHINE}.tar.xz" ]; then
-	FULLPATH="${SRCDIR}/${1}-${MACHINE}.tar.xz"
+    FULLPATH="${SRCDIR}/${1}-${MACHINE}.tar.xz"
 elif [ -f "${SRCDIR}/${1}-image-${MACHINE}.tar.xz" ]; then
-	FULLPATH="${SRCDIR}/${1}-image-${MACHINE}.tar.xz"
+    FULLPATH="${SRCDIR}/${1}-image-${MACHINE}.tar.xz"
 else
-	echo "Rootfs image file not found."
-	echo "Tried the following:"
-	echo "${1}"
-	echo "${SRCDIR}/${1}"
-	echo "${SRCDIR}/${1}-${MACHINE}.tar.xz"
-	echo "${SRCDIR}/${1}-image-${MACHINE}.tar.xz"
-	exit 1
+    echo "Rootfs image file not found."
+    echo "Tried the following:"
+    echo "${1}"
+    echo "${SRCDIR}/${1}"
+    echo "${SRCDIR}/${1}-${MACHINE}.tar.xz"
+    echo "${SRCDIR}/${1}-image-${MACHINE}.tar.xz"
+    exit 1
 fi
- 
+
 if [ "x${2}" = "x" ]; then
         TARGET_HOSTNAME=$MACHINE
 else
@@ -68,20 +68,20 @@ echo "Mounting $DEV as /media"
 mount $DEV /media
 
 echo "Extracting ${FULLPATH} to  /media"
-tar -C /media -xJf ${FULLPATH}
+EXTRACT_UNSAFE_SYMLINKS=1 tar -C /media -xJf ${FULLPATH}
 
 echo "Writing hostname to /etc/hostname"
 export TARGET_HOSTNAME
-echo ${TARGET_HOSTNAME} > /media/etc/hostname        
+echo ${TARGET_HOSTNAME} > /media/etc/hostname
 
 if [ -f ${SRCDIR}/interfaces ]; then
-	echo "Writing interfaces to /media/etc/network/"
-	cp ${SRCDIR}/interfaces /media/etc/network/interfaces
+    echo "Writing interfaces to /media/etc/network/"
+    cp ${SRCDIR}/interfaces /media/etc/network/interfaces
 fi
 
 if [ -f ${SRCDIR}/wpa_supplicant.conf ]; then
-	echo "Writing wpa_supplicant.conf to /media/etc/"
-	cp ${SRCDIR}/wpa_supplicant.conf /media/etc/wpa_supplicant.conf
+    echo "Writing wpa_supplicant.conf to /media/etc/"
+    cp ${SRCDIR}/wpa_supplicant.conf /media/etc/wpa_supplicant.conf
 fi
 
 echo "Unmounting $DEV"
